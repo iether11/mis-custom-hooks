93bef58bddc6eeda7cca6879668d73560da984da
wilver Guzman <wilverguzmanarg@gmail.com>
2022-06-01 19:49:52 -0300

    hooks de React

---
+++ useCounter.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export const useCounter = (initialState=0) => {
+    const [Counter, setCounter] = useState(initialState);
+
+    const increment = () =>{
+        setCounter( Counter + 1 );
+    }
+
+    
+    const decrement = () =>{
+        setCounter( Counter - 1 );
+    }
+    const reset = () => {setCounter(initialState)}
+    
+    return {
+        Counter,
+        increment,
+        decrement,
+        reset,
+        setCounter
+    };
+}
+ 
